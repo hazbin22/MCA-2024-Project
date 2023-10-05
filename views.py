c291ec96deb0818e45dc056622408d1cac592faf
Fathima Hazbin R <118060642+hazbin22@users.noreply.github.com>
2023-10-05 12:56:57 +0530

    Add files via upload
    modified

--- views.py
+++ views.py
@@ -1,28 +1,69 @@
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
-from .models import User
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.auth import login
+#from .models import CustomUser
 
 # Create your views here.
 def home(request):
     return render(request,'index.html')
 def loginn(request):
+    # # if request.method == "POST":
+    # #     username = request.POST['username']
+    # #     password = request.POST['password']
+    # #     user = CustomUser.objects.get(username=username,password=password)
+        
+    # #     if user is not None:
+    # #         # Authentication successful
+    # #         login(request, user)  # Log the user in
+    # #         messages.success(request, "Login successful")
+
+    # #         if user.role == 'S':
+    # #             return redirect('staff')
+    # #         elif user.role == 'C':
+    # #             return redirect('customer')
+    # #         elif user.role == 'D':
+    # #             return redirect('delivery')
+    # #     else:
+    # #         messages.error(request, "Login failed. Please check your credentials.")
+    #         print("Authentication failed for user:", username)
+            
+
+    #return render(request,'index.html')
     return render(request,'login.html')
+
+
+
+
+
+
+
+
 def register(request):    
-    if request.method=="POST":
-        username=request.POST.get("username")
-        name=request.POST.get("name")
-        email=request.POST.get("email")
-        phno=request.POST.get("phone")
-        password=request.POST.get("password")
-        confirm_pass=request.POST.get("confirm_password")
-        if password == confirm_pass:
-            if User.objects.filter(username=username).exists() or User.objects.filter(email=email).exists():
-                return redirect('register')  # 'register' is the name of your registration URL pattern
-            else:
-                myuser= User(name=name,email=email,phno=phno,username=username,password=password,confirm_pass=confirm_pass)
-                myuser.save()
-                return redirect('login')
-        else:
-            return redirect('register')
+    # if request.method=="POST":
+    #     username=request.POST.get("username")
+    #     fname=request.POST.get("fname")
+    #     lname=request.POST.get("lname")
+    #     email=request.POST.get("email")
+    #     password=request.POST.get("password")
+    #     confirm_pass=request.POST.get("confirm_password")
+    #     role=request.POST.get("role")
+    #     if password == confirm_pass:
+    #         if CustomUser.objects.filter(username=username).exists() or CustomUser.objects.filter(email=email).exists():
+    #             return redirect('register')  # 'register' is the name of your registration URL pattern
+    #         else:
+    #             myuser= CustomUser(fname=fname,lname=lname,email=email,username=username,password=password,confirm_pass=confirm_pass,role=role)
+    #             myuser.save()
+    #             return redirect('login')
+    #     else:
+    #         return redirect('register')
     return render(request,'register.html')
 
+def staff(request):
+    return render(request,'staff.html')
+
+def customer(request):
+    return render(request,'customer.html')
+
+def delivery(request):
+    return render(request,'delivery.html')
