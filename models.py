8d41521049337a069c43d1ab4ecc73cf772ccc78
Fathima Hazbin R <118060642+hazbin22@users.noreply.github.com>
2023-10-05 12:56:06 +0530

    Add files via upload
    models modified

--- models.py
+++ models.py
@@ -1,13 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
-class User(models.Model):
-    username=models.CharField(max_length=50)
-    name=models.CharField(max_length=50)
-    email=models.EmailField()
-    phno=models.CharField(max_length=12, blank=True, null=True)
-    password=models.CharField(max_length=50)
-    confirm_pass=models.CharField(max_length=50)
-    def __str__ (self):
-        return self.name,self.email,self.phno,self.password
-    
+
+class RegisteredUsers(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+class RegisteredUsers(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    house = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    place = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    pincode = models.CharField(max_length=6)
+    phone = models.CharField(max_length=10)
+    password = models.CharField(max_length=50)
+    confirm_password = models.CharField(max_length=50)
+    ROLE_CHOICES = [
+        ('S', 'Staff'),
+        ('C', 'Customer'),
+        ('D', 'Delivery Team'),
+    ]
+    role = models.CharField(max_length=1, choices=ROLE_CHOICES, default='C')
+
+    objects = RegisteredUsers()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['email','fname','lname']
+
+    def __str__(self):
+        return self.email
