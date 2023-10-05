958193767bb13dc90ad2d4fea8d76f0a6aa5e91f
Fathima Hazbin R <118060642+hazbin22@users.noreply.github.com>
2023-10-05 12:56:32 +0530

    Add files via upload
    modified

--- admin.py
+++ admin.py
@@ -1,6 +1,16 @@
+# from django.contrib import admin
+# from .models import RegisteredUsers
+
+# # Register your models here.
+
+# class UserAdmin(admin.UserAdmin):
+#     list_display =('email','fname', 'lname','date_of_birth','house','street', 'place', 'district', 'pincode', 'phone', 'password', 'confirm_password', 'role')
+
+# admin.site.register(RegisteredUsers,UserAdmin)
 from django.contrib import admin
+from .models import RegisteredUsers
 
-# Register your models here.
-from .models import User
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'fname', 'lname', 'date_of_birth', 'house', 'street', 'place', 'district', 'pincode', 'phone', 'password', 'role')
 
-admin.site.register(User)
+admin.site.register(RegisteredUsers, UserAdmin)
